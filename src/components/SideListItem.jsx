dabe7cc8ec9f12dd40ed057605cd7afaf5fa90a6
Linlin Li <paris1900lll@gmail.com>
2021-06-17 18:22:51 +0100

    chanllenge one completed

--- src/components/SideListItem.jsx
+++ src/components/SideListItem.jsx
@@ -1,15 +1,21 @@
-
-export default function SideListItem({ isSelectedCripto, setSelectedCripto, item,selectedCripto,currentPriceObj,setCurrentPriceObj }) {
+export default function SideListItem({
+  isSelectedCripto,
+  setSelectedCripto,
+  item,
+  selectedCripto,
+  currentPriceObj,
+  setCurrentPriceObj,
+}) {
   const { id, name } = item;
-
-  
 
   return (
     <li>
       <button
         className={isSelectedCripto(id) ? "selected" : ""}
         onClick={() => {
-          selectedCripto===id? setSelectedCripto(false) :setSelectedCripto(id)
+          selectedCripto === id
+            ? setSelectedCripto(false)
+            : setSelectedCripto(id);
         }}
       >
         {name}
